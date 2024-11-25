f44c2956be15927b40db2abf3c56ad95c92e6471
Ritviz01 <174640068+Ritviz01@users.noreply.github.com>
2024-11-25 15:17:28 +0000

    learned about varianbles

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 12346
+let accountEmail = "ritviztiwari07@gmail.com"
+var accountPass = 45463
+accountcity="Kota"
+//const cannot be changed 
+/*Var variable is not used in modern codes as it has the problem of block scope */
+console.table([accountId, accountEmail, accountPass, accountcity]);
