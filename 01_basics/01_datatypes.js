dfdabab3ed8d8b23758eccc3df6e92333b0b3d46
Ritviz01 <174640068+Ritviz01@users.noreply.github.com>
2024-11-26 16:00:53 +0000

    datatypes done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,11 @@
+"use strict";// treat all JS code as newer version
+//readability should be good
+
+
+// number => 2 to the power 53
+// bigint for larger numbers
+//string => " "
+// boolean =>true/false
+//null: it's  a standalone value
+//undefined  
+//symbol
