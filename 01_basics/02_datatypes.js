ee9c692c3faf009571220dee7693e255f086d4e8
Ritviz01 <174640068+Ritviz01@users.noreply.github.com>
2024-11-27 12:58:56 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";// treat all JS code as newer version
+//readability should be good
+//alert (3+3)//it is node js not browser
+
+
+
+
+let name="ritviz"
+let loggedin = false
+let age =18
+
+
+// number => 2 to the power 53
+// bigint for larger numbers
+// string => " "
+// boolean =>true/false
+// null: it's  a standalone value
+// undefined  
+// symbol=> unique
+// objects 
+
+console.log(typeof  "string");
