96216be78d091b89985adac6752ab5904c6b4d3e
Ritviz01 <174640068+Ritviz01@users.noreply.github.com>
2024-11-27 13:42:09 +0000

    conversion operation

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -8,8 +8,18 @@ let valueInNumber = Number(score)
 // true => 1; false => 0
 
 let isLoggedIn= 1
-let booleanisLoggedIn=Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+// let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
 // 1=> true ;0=> false
 //""=>false
 // "ritviz"=>true
+
+let somenumber =45
+let stringNumber = String(somenumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//***************Operations***************
+let value =3
+let negvalue=-value
+ 
