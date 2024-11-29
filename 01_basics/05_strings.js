6c5b4a060cd82b9f1e801e0282840b6521d2b148
Ritviz01 <174640068+Ritviz01@users.noreply.github.com>
2024-11-29 13:46:19 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -4,6 +4,28 @@
 //console.log(name+repoCount+"Value");
 
 //console.log(`heloo this side ${name} my repo's are ${repoCount}`);
-const gameName = new String('Ritviz')
-console.log(gameName[0]);
-  
+const gameName = new String('ritvizvghsdsfg-ashdgfas-adsf')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString  = gameName.substring(0,5)
+// console.log(newString);
+
+
+const anotherString=gameName.slice(-5)//we can provide neg values
+// console.log(anotherString);
+
+const newStrirngOne="  ritviz  "
+// console.log(newStrirngOne);
+// console.log(newStrirngOne.trim());
+
+const ulr ='https.//alookisabji.com/rugby%50choudhary'
+console.log(ulr.replace('%50', '-'));
+console.log(ulr.includes('alookisabji' ));
+console.log(gameName.split('-'));
