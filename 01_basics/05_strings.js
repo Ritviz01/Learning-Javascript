71c2412fb45a008dbde94f59a938ccdbe6e788e6
Ritviz01 <174640068+Ritviz01@users.noreply.github.com>
2024-11-29 13:08:53 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+// const name="ritviz"
+// const repoCount = 50
+
+//console.log(name+repoCount+"Value");
+
+//console.log(`heloo this side ${name} my repo's are ${repoCount}`);
+const gameName = new String('Ritviz')
+console.log(gameName[0]);
+  
