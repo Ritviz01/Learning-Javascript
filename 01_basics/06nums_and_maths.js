1401b1c5dac7161bbbd6652498be59690a8294a0
Ritviz01 <174640068+Ritviz01@users.noreply.github.com>
2024-11-30 11:26:34 +0000

    nums and maths

---
+++ 01_basics/06nums_and_maths.js
@@ -0,0 +1,25 @@
+const balance = new Number(76)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+
+//console.log(balance.toFixed(1));
+
+const diffNo=55485.2144230
+
+//console.log(diffNo.toPrecision(4));
+
+const hund= 100000000
+// console.log(hund.toLocaleString('en-IN'));
+
+
+// ***********************Maths***********************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.9));
+
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+
