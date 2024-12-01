ab214184e9be2fbcceb5018cd0ed541436e8de88
Ritviz01 <174640068+Ritviz01@users.noreply.github.com>
2024-12-01 15:26:21 +0000

    numbers and maths

--- 01_basics/06nums_and_maths.js
+++ 01_basics/06nums_and_maths.js
@@ -21,5 +21,20 @@ const hund= 100000000
 
 // console.log(Math.ceil(4.2));
 // console.log(Math.floor(4.9));
+// console.log(Math.min(4.9,23,234));
+// console.log(Math.max(4.9,234,554));
+
+console.log(Math.random());
+
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
 
 
